4a5dcb71ca086c88b1a699d15168ad5faaf8b5db
tylerbang <tylerbang@protonmail.com>
2024-04-27 18:10:35 -0500

    Argh... we are so close now...

--- example/homework/02/init.cpp
+++ example/homework/02/init.cpp
@@ -58,20 +58,42 @@ int main(int argc, char* argv[]) {
 
 		// Check if the number of rows is divisible by the number of processes
 		int nRowsLocal = nRows / size;
-		if (rank == (size-1)){
-			nRowsLocal += nRows % size;
-		}
+		int remainder = nRows % size;
 
-		// print the number of rows local
-		std::cout << "Number of rows local: " << nRowsLocal << std::endl;
+		if (rank < remainder) {
+			nRowsLocal++;
+		}
 
 		// the ghost cells are the cells that are on the edge of the domain
 		int nRowsLocalWithGhost = nRowsLocal + 2;
 		int nColsWithGhost = nCols + 2;
 
 		// Initialize the domain (remember, we are using Kokkos views)
-		Kokkos::View<int**> currDomain("currDomain", nRowsLocalWithGhost, nColsWithGhost);
-		Kokkos::View<int**> nextDomain("nextDomain", nRowsLocalWithGhost, nColsWithGhost);
+		Kokkos::View<int**> currDomain("currDomain", nRowsLocal + 2, nCols + 2);
+		Kokkos::View<int**> nextDomain("nextDomain", nRowsLocal + 2, nCols + 2);
+
+		// fill the domain with random values
+		srand(time(0) + rank);
+		for (int iRow = 1; iRow <= nRowsLocal; iRow++) {
+			for (int iCol = 1; iCol <= nCols; iCol++) {
+				currDomain(iRow, iCol) = rand() % 2;
+			}
+		}
+
+		int* sendcounts = new int[size];
+		int* displs = new int[size];
+
+		for (int i = 0; i < size; i++) {
+    		sendcounts[i] = (nRows / size + (i < remainder ? 1 : 0)) * nCols;
+    		displs[i] = (i * nRows / size + std::min(i, remainder)) * nCols;
+		}
+		if (rank == 0) {
+    		MPI_Scatterv(&currDomain(1, 1), sendcounts, displs, MPI_INT, MPI_IN_PLACE, 0, MPI_INT, 0, comm);
+		} else {
+    		MPI_Scatterv(NULL, NULL, NULL, MPI_INT, &currDomain(1, 1), nRowsLocal * nCols, MPI_INT, 0, comm);
+		}
+		delete[] sendcounts;
+		delete[] displs;
 
 		int dims[2] = {0, 0};
 		MPI_Dims_create(size, 2, dims);
@@ -92,34 +114,41 @@ int main(int argc, char* argv[]) {
 		int northwest, northeast, southwest, southeast;
 		MPI_Cart_shift(comm2D, 0, 1, &northwest, &southeast);
 		MPI_Cart_shift(comm2D, 1, 1, &southwest, &northeast);
-
-		// fill in the domain with random values, using Kokkos parallel_for
-		if (rank == 0){
-			srand(time(0));
-			for (int iRow = 1; iRow <= nRows; iRow++) {
-				for (int iCol = 1; iCol <= nCols; iCol++) {
-					currDomain(iRow, iCol) = rand() % 2;
-				}
-			}
-		}
-
-		// then broadcast the domain to all the other processes
-		MPI_Bcast(&currDomain(1, 1), nRows * nCols, MPI_INT, 0, comm);
+		MPI_Cart_shift(comm2D, 0, 1, &northwest, &northeast);
+		MPI_Cart_shift(comm2D, 1, 1, &southwest, &southeast);
 
 		const int ALIVE = 1;
 		const int DEAD = 0;
 
+		// vscode is a slow piece of garbage
+		// I want to use emacs
+		// but sadly I cannot because there
+		// is no copilot for emacs
+		// sadge
+
+		// Brock Purdy is the greatest Mr. Irrelevant of all time
+
 		// now we can start the simulation
 		for (int iTime = 0; iTime < nTime; iTime++) {
 
-			MPI_Sendrecv(&currDomain(1, 1), nCols, MPI_INT, north, 0, &currDomain(nRowsLocal + 1, 1), nCols, MPI_INT, south, 0, comm2D, MPI_STATUS_IGNORE);
-			MPI_Sendrecv(&currDomain(nRowsLocal, 1), nCols, MPI_INT, south, 0, &currDomain(0, 1), nCols, MPI_INT, north, 0, comm2D, MPI_STATUS_IGNORE);
-			MPI_Sendrecv(&currDomain(1, 1), 1, MPI_INT, west, 0, &currDomain(1, nCols + 1), 1, MPI_INT, east, 0, comm2D, MPI_STATUS_IGNORE);
-			MPI_Sendrecv(&currDomain(1, nCols), 1, MPI_INT, east, 0, &currDomain(1, 0), 1, MPI_INT, west, 0, comm2D, MPI_STATUS_IGNORE);
-			MPI_Sendrecv(&currDomain(1, 1), 1, MPI_INT, northwest, 0, &currDomain(nRowsLocal + 1, nCols + 1), 1, MPI_INT, southeast, 0, comm2D, MPI_STATUS_IGNORE);
-			MPI_Sendrecv(&currDomain(1, nCols), 1, MPI_INT, northeast, 0, &currDomain(nRowsLocal + 1, 0), 1, MPI_INT, southwest, 0, comm2D, MPI_STATUS_IGNORE);
-			MPI_Sendrecv(&currDomain(nRowsLocal, 1), 1, MPI_INT, southwest, 0, &currDomain(0, nCols + 1), 1, MPI_INT, northeast, 0, comm2D, MPI_STATUS_IGNORE);
-			MPI_Sendrecv(&currDomain(nRowsLocal, nCols), 1, MPI_INT, southeast, 0, &currDomain(0, 0), 1, MPI_INT, northwest, 0, comm2D, MPI_STATUS_IGNORE);
+				// convert to separate sends and receives
+				MPI_Send(&currDomain(1, 1), nCols, MPI_INT, north, 0, comm2D);
+				MPI_Send(&currDomain(nRowsLocal, 1), nCols, MPI_INT, south, 0, comm2D);
+				MPI_Send(&currDomain(1, 1), 1, MPI_INT, west, 0, comm2D);
+				MPI_Send(&currDomain(1, nCols), 1, MPI_INT, east, 0, comm2D);
+				MPI_Send(&currDomain(1, 1), 1, MPI_INT, northwest, 0, comm2D);
+				MPI_Send(&currDomain(1, nCols), 1, MPI_INT, northeast, 0, comm2D);
+				MPI_Send(&currDomain(nRowsLocal, 1), 1, MPI_INT, southwest, 0, comm2D);
+				MPI_Send(&currDomain(nRowsLocal, nCols), 1, MPI_INT, southeast, 0, comm2D);
+
+				MPI_Recv(&currDomain(nRowsLocal + 1, 1), nCols, MPI_INT, south, 0, comm2D, MPI_STATUS_IGNORE);
+				MPI_Recv(&currDomain(0, 1), nCols, MPI_INT, north, 0, comm2D, MPI_STATUS_IGNORE);
+				MPI_Recv(&currDomain(1, nCols + 1), 1, MPI_INT, east, 0, comm2D, MPI_STATUS_IGNORE);
+				MPI_Recv(&currDomain(1, 0), 1, MPI_INT, west, 0, comm2D, MPI_STATUS_IGNORE);
+				MPI_Recv(&currDomain(nRowsLocal + 1, nCols + 1), 1, MPI_INT, southeast, 0, comm2D, MPI_STATUS_IGNORE);
+				MPI_Recv(&currDomain(nRowsLocal + 1, 0), 1, MPI_INT, southwest, 0, comm2D, MPI_STATUS_IGNORE);
+				MPI_Recv(&currDomain(0, nCols + 1), 1, MPI_INT, northeast, 0, comm2D, MPI_STATUS_IGNORE);
+				MPI_Recv(&currDomain(0, 0), 1, MPI_INT, northwest, 0, comm2D, MPI_STATUS_IGNORE);
 
 			// updoot domainy boi
 			Kokkos::parallel_for("updateDomain", Kokkos::MDRangePolicy<Kokkos::Rank<2>>({1, 1}, {nRowsLocalWithGhost, nColsWithGhost}), KOKKOS_LAMBDA (const int iRow, const int iCol) {
@@ -168,3 +197,5 @@ int main(int argc, char* argv[]) {
   	Kokkos::finalize();
 	MPI_Finalize();
 }
+
+// I hate M
